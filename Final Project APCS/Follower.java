c181a746d36a5b9a596f4dd29e622b76d0ac0e9b
lmaiora7 <lmaiora7@students.d125.org>
2015-05-15 09:00:17 -0500

    added a lot of work

--- Final Project APCS/Follower.java
+++ Final Project APCS/Follower.java
@@ -7,21 +7,66 @@
  */
 public class Follower
 {
-    private Point currentTarget;
     private ObjectMap currentMap;
+    private Point currentTarget=null;
+    private Point finalTarget;
     private Point loc;
+    private double m;
+    private double b;
     public Follower(int xint,int yint,ObjectMap o)
     {
         loc=new Point(xint,yint);
         currentMap=o;
     }
     
-    public void setTarget(int xx, int yy){currentTarget=new Point(xx,yy);}
+    /**
+     * This method changes the main target manually
+     */
+    public void setTarget(int xx, int yy){finalTarget=new Point(xx,yy);}
+    
+    /**
+     * This method scans whats the next step to getting to the finalTarget by making a new currentTarget
+     */
+    public void findNextTarget(){
+        //needs to be added
+    }
+    
+    /**
+     * This method takes the currentTarget and loc to create a equation for getting to the currentTarget
+     */
+    public void findNextDirection(){
+        m=(currentTarget.ycord()-loc.ycord())/(currentTarget.xcord()-loc.xcord());
+        b=loc.ycord()/(loc.xcord()*m);
+    }
     
     /**
      * This method updates the follower, by consectutively calling this it will move to its objective
      */
     public void update(){
-        
+        if(currentTarget==null || loc.distance(finalTarget)<5){
+            //do nothing due to having no target or are currently at the final destination
+        }
+        else if(loc.distance(currentTarget)<5){
+            findNextTarget();
+            findNextDirection();
+        }
+        else{
+            if(currentTarget.xcord()>loc.xcord()){
+                loc.xadd(1);
+                loc.yset((int)(m*loc.xcord()+b));
+            }
+            else if(currentTarget.xcord()<loc.xcord()){
+                loc.xadd(-1);
+                loc.yset((int)(m*loc.xcord()+b));
+            }
+            else{
+                if(currentTarget.ycord()>loc.ycord()){
+                    loc.yadd(1);
+                }
+                else if(currentTarget.ycord()<loc.ycord()){
+                    loc.yadd(-1);
+                }
+            }
+        }
     }
 }
--- Final Project APCS/Object.java
+++ Final Project APCS/Object.java
@@ -10,14 +10,19 @@ public class Object
     private Point loc;
     private int length;
     private int height;
-    
+    private Point pt1,pt2,pt3,pt4; //These points are for the follower to track and help them find a path
     public Object(int xx,int yy,int len,int hei)
     {
         loc.xset(xx);
         loc.yset(yy);
         length=len;
         height=hei;
+        //  1_____2
+        //  |_____|
+        //  3     4
+        pt1=new Point(xx-5,yy-5);
+        pt2=new Point(xx+length+5,yy-5);
+        pt3=new Point(xx-5,yy+height+5);
+        pt4=new Point(xx+length+5,yy+height+5);
     }
-
-    
 }
